8635ae1d3c869fb1923b061b7d0766a2ec9f7200
Agostina Soto <118022009+agossoto@users.noreply.github.com>
2023-11-13 17:36:10 -0300

    Nifti y Excel
    Excel de pacientes con posibles errores o cosas a mejorar??
    Copia de  nifti fuera de sus carpetas

--- TP_Final- Separado en funciones/TP_FInal.m
+++ TP_Final- Separado en funciones/TP_FInal.m
@@ -13,10 +13,13 @@ clc
 % a=squeeze(a);
 % info=dicominfo("Paciente_5_ct/ADNI_067_S_1185_MR_MPRAGE_br_raw_20070727145737734_98_S36449_I62464.dcm");
 
-niifo=niftiinfo('brainmask.nii');
-V = niftiread('brainmask.nii');
+niifo=niftiinfo('067_S_1185_brainmask.nii');
+V = niftiread('067_S_1185_brainmask.nii');
 k=4;
-img_spacing=1;
-mm_px=1;
+img_spacing=1.2;
+mm_px=1.3;
 
 [Vol_Liq, Vol_GrayM, Vol_WhiteM] = Calculate_volumes(V, k, img_spacing,mm_px);
+
+[Vol_Liq Vol_GrayM Vol_WhiteM] % Lo usé para copiar en excel
+%volumeViewer(V) % visualizar en 3d 
